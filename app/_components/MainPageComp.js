e1bfec0dbb62a6db2a129fd407ed842e054ec9e8
Aliniroomand <aliniroomand32@gmail.com>
2024-08-14 04:13:00 +0330

    create Banner without responsive

--- app/_components/MainPageComp.js
+++ app/_components/MainPageComp.js
@@ -9,6 +9,9 @@ import flower from "@/src/assets/images/Root/flower_transparent.png"
 import fruit from "@/src/assets/images/Root/fruit_transparent.png"
 import contactUs from "@/src/assets/images/Root/contactUs_transparent.png"
 import learning from "@/src/assets/images/Root/teach_transparent.png"
+import rope from "@/src/assets/images/Root/Rope.png"
+import stick from "@/src/assets/images/Root/stick.png"
+import example from "@/src/assets/images/Root/new_design_with_flower___.jpg"
 
 
 
@@ -17,10 +20,10 @@ export default function MainPageComp(){
 return(
     <>
 
-    <main  className='relative '>
+    <main  className='relative flex flex-col justify-between items-center'>
         {/* Background */}
         <Image
-            className=' fixed h-full z-[1] w-screen right-0 bottom-0 object-cover object-left-bottom blur-[1px]'
+            className=' fixed h-full z-[-1] w-full right-0 bottom-0 object-cover object-left-bottom blur-[1px]'
             src={BG}
             placeholder='blur'
             quality={10}
@@ -28,12 +31,47 @@ return(
         />
 
         {/* Title Part */}
-            <p className='fixed z-10 h-[13svh] sm:h-[15svh] top-0 sm:text-xl text-xl text-center bg-white backdrop-blur-[1px] bg-opacity-55 text-slate-900 w-full font-semibold sm:py-2 py-6 text_animation' style={{ textShadow: '0px 0px 5px #fff' }}>
+            <p className='fixed z-30 h-[13svh] sm:h-[15svh] top-0 sm:text-xl text-xl text-center bg-white backdrop-blur-[1px] bg-opacity-85 text-slate-900 w-full font-semibold sm:py-2 py-6 text_animation' style={{ textShadow: '0px 0px 5px #fff' }}>
                 به دنیای رنگی دیزاین لند خوش اومدی <br />برای دیدن قسمتهای مختلف هنر ما اسکرول کن
             </p>
+        {/*___________Title Part */}
+
+        {/* Banner part  */}
+        <article className='relative w-full h-[40svh] top-[18svh] py-3 text-center overflow-visible '>
+    <h1 className='text-2xl font-bold h-10'>
+        پرطرفدار ها
+    </h1>
+    <Image className='absolute w-full top-12 scale-y-150 right-0 z-[19] ' src={rope} alt='banner rope' />
+    <section className='absolute top-10 flex flex-row gap-2 text-nowrap overflow-x-scroll overflow-visible w-full  '>
+
+
+        <section className='relative slidingForBanner_animation moveZ flex-none w-32 h-44 top-18 z-[20] overflow-visible'>
+            <Image src={stick} className='absolute  w-4 z-[70]  mx-14' alt='image sticker' />
+            <Image src={example} className='absolute w-full rounded-2xl neo_shadow h-4/5 bottom-0' alt='example' />
+        </section>
+        <section className='relative slidingForBanner_animation moveZ flex-none w-32 h-44 top-18 z-[20] overflow-visible'>
+            <Image src={stick} className='absolute  w-4 z-[70]  mx-14' alt='image sticker' />
+            <Image src={example} className='absolute w-full rounded-2xl neo_shadow h-4/5 bottom-0' alt='example' />
+        </section>
+        <section className='relative slidingForBanner_animation moveZ flex-none w-32 h-44 top-18 z-[20] overflow-visible'>
+            <Image src={stick} className='absolute  w-4 z-[70]  mx-14' alt='image sticker' />
+            <Image src={example} className='absolute w-full rounded-2xl neo_shadow h-4/5 bottom-0' alt='example' />
+        </section>
+        <section className='relative slidingForBanner_animation moveZ flex-none w-32 h-44 top-18 z-[20] overflow-visible'>
+            <Image src={stick} className='absolute  w-4 z-[70]  mx-14' alt='image sticker' />
+            <Image src={example} className='absolute w-full rounded-2xl neo_shadow h-4/5 bottom-0' alt='example' />
+        </section>
+        <section className='relative slidingForBanner_animation moveZ flex-none w-32 h-44 top-18 z-[20] overflow-visible'>
+            <Image src={stick} className='absolute  w-4 z-[70]  mx-14' alt='image sticker' />
+            <Image src={example} className='absolute w-full rounded-2xl neo_shadow h-4/5 bottom-0' alt='example' />
+        </section>
+
+    </section>
+</article>
+        {/*__________Banner part  */}
 
         {/* Selecting Part */}
-        <section className='relative z-20 w-svw h-[87svh] top-[13svh] overflow-y-scroll overflow-x-hidden lg:overflow-hidden flex flex-col lg:flex-row lg:items-center lg:justify-stretch gap-[20svh]'>
+        <section className='relative z-20 w-full h-[87svh] top-[13svh] overflow-y-scroll overflow-x-hidden lg:overflow-hidden flex flex-col lg:flex-row lg:items-center lg:justify-stretch gap-[20svh]'>
             <Link 
                 href="balloon"
                 className="relative z-30 w-full h-[50svh] wobble grid place-items-center " >
